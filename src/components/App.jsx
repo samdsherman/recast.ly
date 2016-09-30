d8f18990e8c8ad8cde2431216c05057362d9f49f
samdsherman <samdsherman@gmail.com>
2016-09-30 16:50:28 -0700

    Make title clicking work with stateless children

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,33 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer video={exampleVideoData[0]}/>
+class App extends React.Component { 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentVideo: exampleVideoData[0]
+    };
+  }
+
+  videoChange(video) {
+    this.setState({
+      currentVideo: video
+    });
+  }
+
+  render() {
+    return (
+    <div>
+      <Nav />
+      <div className="col-md-7">
+        <VideoPlayer video={this.state.currentVideo}/>
+      </div>
+      <div className="col-md-5">
+        <VideoList videos={exampleVideoData} clickHandler={video=>this.videoChange(video)}/>
+      </div>
     </div>
-    <div className="col-md-5">
-      <VideoList videos={exampleVideoData}/>
-    </div>
-  </div>
-);
+    );
+  }
+}
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
