89692e71cf5bcee53e406d7ed5a42d896ee8e508
samdsherman <samdsherman@gmail.com>
2016-09-30 15:12:01 -0700

    Pass all VideoPlayer tests

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ var App = () => (
   <div>
     <Nav />
     <div className="col-md-7">
-      <VideoPlayer/>
+      <VideoPlayer video={exampleVideoData[0]}/>
     </div>
     <div className="col-md-5">
       <VideoList videos={exampleVideoData}/>
