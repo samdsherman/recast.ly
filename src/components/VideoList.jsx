789848ca3cac21a44f1eb03b1599f7af164fdea9
samdsherman <samdsherman@gmail.com>
2016-09-30 15:54:21 -0700

    Enable clicking on title to change video

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,8 +1,16 @@
-var VideoList = (props) => (
-  <div className="video-list media">
-    {props.videos.map(video => <VideoListEntry video={video} />)}
-  </div>
-);
+class VideoList extends React.Component {
+  handleClick(video) {
+    this.props.onVideoChange(video);
+  }
+
+  render() {
+    return (
+      <div className="video-list media">
+        {this.props.videos.map(video => <VideoListEntry video={video} onVideoClick={this.handleClick.bind(this)} />)}
+      </div>
+    );
+  }
+}
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
